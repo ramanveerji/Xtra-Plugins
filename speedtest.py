0e8960f1fe58148671b207767979db2780c65230
RamandeepS <70764724+ramanveerji@users.noreply.github.com>
2022-05-05 00:47:29 +0530

    Update speedtest.py

--- speedtest.py
+++ speedtest.py
@@ -1,4 +1,4 @@
-import speedtest
+from speedtest import Speedtest
 from main_startup.core.decorators import friday_on_cmd
 from main_startup.helper_func.basic_helpers import edit_or_reply, get_text, edit_or_send_as_file, get_user
 
